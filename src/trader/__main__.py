f90a1aedaa4555ac5c9314fdc6b7acabf31d7c34
saltchicken <john.eicher89@gmail.com>
2025-06-28 15:12:58 -0700

    Better update symbols and symbol return list

--- src/trader/__main__.py
+++ src/trader/__main__.py
@@ -24,13 +24,22 @@ class Trader:
         self.client = FinanceClient()
         self.db = DatabaseClient("stock_data")
 
-    # TODO: I want this to check if each symbol doesn't already exist in the database
     def update_symbols(self):
         symbols = self.client.get_all_stocks()
-        self.db.update_symbols(symbols)
+        new_symbols = []
+
+        for symbol in symbols:
+            if not self.db.does_symbol_exist(symbol):
+                new_symbols.append(symbol)
+
+        if new_symbols:
+            print(f"Adding {len(new_symbols)} new symbols.")
+            self.db.update_symbols(new_symbols)
+        else:
+            print("No new symbols to add.")
 
     def daily_update(self):
-        for symbol in self.client.get_all_stocks():
+        for symbol in self.db.get_all_symbols():
             print(symbol)
             if self.db.was_updated_today(symbol):
                 print(f"{symbol} already updated today. Skipping.")
@@ -44,13 +53,13 @@ if __name__ == "__main__":
     trader = Trader()
     trader.update_symbols()
 
-    trader.daily_update()
+    # trader.daily_update()
 
     # db.add_new_column("stock", "three_month_average_trading_volume", "FLOAT")
 
-    trader.db.print_table("companies")
-    trader.db.print_table("metric_snapshots")
-    trader.db.print_table("current_metrics")
+    # trader.db.print_table("companies")
+    # trader.db.print_table("metric_snapshots")
+    # trader.db.print_table("current_metrics")
 
     def quote_history_test():
         data = trader.client.get_quote_history("T", "10y")
