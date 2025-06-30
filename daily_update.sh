633ce8808adb5ed2ee5d3f9235f98a4f44ff5d80
saltchicken <john.eicher89@gmail.com>
2025-06-30 11:18:58 -0700

    Log output to journalctl via systemd-cat

--- daily_update.sh
+++ daily_update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/saltchicken/workspace/trader/.venv/bin/activate
 cd /home/saltchicken/workspace/trader
-python src/trader/daily_update.py >>daily_update.log 2>&1
+python src/trader/daily_update.py 2>&1 | systemd-cat -t daily_update
