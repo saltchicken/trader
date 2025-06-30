3923c44bb2b0d5cad320161a288f7769d729f94e
saltchicken <john.eicher89@gmail.com>
2025-06-29 17:30:08 -0700

    Get latest

--- src/trader/trader.py
+++ src/trader/trader.py
@@ -1,11 +1,14 @@
+from sqlalchemy import func, desc
+from sqlalchemy.orm import aliased
 from finance_client import FinanceClient
-from database import DatabaseClient, Company
+from database import DatabaseClient, Company, MetricSnapshot
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 from pprint import pprint
 
 from log import logger
+
 
 class Trader:
     def __init__(self):
@@ -42,9 +45,35 @@ class Trader:
 
             metrics = self.client.get_metrics(symbol)
             self.db.daily_update(symbol, metrics["metric"])
+
     def is_within_allowed_update_window(self):
         now = datetime.now(ZoneInfo("America/Los_Angeles"))
         if now.hour >= 18 or now.hour < 2:
             return True
         return False
 
+    def get_latest(self, limit=10):
+        subquery = (
+            self.db.session.query(
+                MetricSnapshot.symbol,
+                func.max(MetricSnapshot.timestamp).label("latest"),
+            )
+            .group_by(MetricSnapshot.symbol)
+            .subquery()
+        )
+
+        alias = aliased(MetricSnapshot)
+
+        query = (
+            self.db.session.query(alias.symbol, alias.month3_average_trading_volume)
+            .join(
+                subquery,
+                (alias.symbol == subquery.c.symbol)
+                & (alias.timestamp == subquery.c.latest),
+            )
+            .filter(alias.month3_average_trading_volume != None)
+            .order_by(alias.month3_average_trading_volume.desc())
+            .limit(limit)
+        )
+
+        return query.all()
